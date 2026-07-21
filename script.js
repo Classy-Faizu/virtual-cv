b3664bee22a9f35f4ad922be18c00d189e9884dd
Mohammed Faiz Shootariya <faizshootariya144@gmail.com>
2026-07-21 17:27:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -180,3 +180,89 @@ backToTop.addEventListener("click", () => {
     });
 
 });
+particlesJS("particles-js", {
+
+    particles: {
+
+        number: {
+            value: 70,
+            density: {
+                enable: true,
+                value_area: 900
+            }
+        },
+
+        color: {
+            value: "#38bdf8"
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.35
+        },
+
+        size: {
+            value: 3,
+            random: true
+        },
+
+        line_linked: {
+            enable: true,
+            distance: 160,
+            color: "#38bdf8",
+            opacity: 0.18,
+            width: 1
+        },
+
+        move: {
+            enable: true,
+            speed: 1.8,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out"
+        }
+
+    },
+
+    interactivity: {
+
+        detect_on: "canvas",
+
+        events: {
+
+            onhover: {
+                enable: true,
+                mode: "grab"
+            },
+
+            onclick: {
+                enable: true,
+                mode: "push"
+            }
+
+        },
+
+        modes: {
+
+            grab: {
+                distance: 180,
+                line_linked: {
+                    opacity: 0.45
+                }
+            },
+
+            push: {
+                particles_nb: 4
+            }
+
+        }
+
+    },
+
+    retina_detect: true
+
+});
